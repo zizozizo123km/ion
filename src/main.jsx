bbc2667bc4dc0c46fd8af8fff4e316b2de430dc2
zizozizo123km <nnacar12@gmail.com>
2026-01-04 17:54:51 +0100

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css' // Tailwind base styles
+import './App.css' // Custom styles (critical rule 2)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
